f2c43939cbd7774741e9ade2fbae3f5c3b0a66c7
RAVI RAJ <raviraj2k09@gmail.com>
2026-09-22 13:48:22 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+    data:    require('./data'),
+    colors:  require('./colors'),
+    render:  require('./render'),
+    copy:    require('./copy'),
+    help:    require('./help'),
+    tw:      require('./typewriter')
+};
